4a2f444fd01d765f9e94c247cc1f5fd2dc5292da
Markus Wolf <mail@markus-wolf.de>
2016-10-16 10:21:27 +0200

    refactor: restructured build function

--- index.js
+++ index.js
@@ -10,20 +10,25 @@ module.exports = {
 	build
 };
 
-function logError(err) {
-	console.error(`\n${chalk.bold.red('FAILED')}\n`);
-	console.error(err);
-	throw err;
+function logResult(promise) {
+	return promise
+		.then(() => {
+			console.log(`\n${chalk.bold.green('SUCCESS')}\n`);
+		})
+		.catch(err => {
+			console.error(`\n${chalk.bold.red('FAILED')}\n`);
+			console.error(err);
+			throw err;
+		});
 }
 
 function build(opts) {
 	const {srcPages, srcTemplates, srcStatics, srcHelpers, dest, destStatics} = opts;
-	return sander.copydir(path.join(process.cwd(), srcStatics)).to(path.join(process.cwd(), destStatics))
-		.catch(() => {/* just ignore missing statics folder */})
-		.then(() => createReactComponents(srcTemplates, srcHelpers))
-		.then(components =>
-			globby([srcPages])
-				.then(filepaths => renderPages(filepaths, components, dest))
-		.then(() => console.log(`\n${chalk.bold.green('SUCCESS')}\n`)))
-		.catch(err => logError(err));
+	const promise =
+		sander.copydir(path.join(process.cwd(), srcStatics)).to(path.join(process.cwd(), destStatics))
+			.catch(() => {/* just ignore missing statics folder */})
+			.then(() => createReactComponents(srcTemplates, srcHelpers))
+			.then(components => globby([srcPages]).then(filepaths => [components, filepaths]))
+			.then(([components, filepaths]) => renderPages(filepaths, components, dest));
+	return logResult(promise);
 }
