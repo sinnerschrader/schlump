d26de5cfb82e84efd4ef0dbfab7affe1a96f7341
Mario Nebl <hello@herebecode.com>
2016-11-26 16:19:12 +0100

    feat: allow multiple srcStatics entries
    - use it by repeating --src-statics=[value]
    
    closes #37

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const path = require('path');
-const globby = require('globby');
-const sander = require('sander');
 const chalk = require('chalk');
+const globby = require('globby');
+const throat = require('throat');
+const sander = require('sander');
 
 const host = require('./host');
 const {renderPages} = require('./pages');
@@ -9,12 +10,49 @@ const {createTemplates} = require('./templates');
 const {createRedirects} = require('./redirects');
 const {mixinExternalTemplates} = require('./external-templates');
 
+const cwd = process.cwd();
+
 module.exports = {
 	build
 };
 
-function logResult(promise) {
-	return promise
+function build(opts) {
+	return Promise.resolve()
+		.then(() => {
+			const destStatics = path.resolve(cwd, opts.destStatics);
+
+			const srcs = Array.isArray(opts.srcStatics) ?
+				opts.srcStatics : [opts.srcStatics];
+			const srcsStatics = srcs.map(src => path.resolve(cwd, src));
+
+			const jobs = srcsStatics.map(throat(1, src => sander.copydir(src).to(destStatics)));
+			return Promise.all(jobs);
+		})
+		.then(() => createTemplates(opts.srcTemplates, opts.srcHelpers, {cssVariables: opts.cssVariables}))
+		.then(templates => mixinExternalTemplates(opts.templateImport, templates))
+		.then(templates =>
+			globby([opts.srcPages])
+				.then(filepaths => [templates, filepaths]))
+		.then(([templates, filepaths]) =>
+			globby([path.join(opts.destStatics, '**')])
+			.then(statics => {
+				// If we need to write css, then implicitly count it to static resources
+				if (opts.scopedCss) {
+					return [...statics, opts.scopedCss.replace(/^.\//, '')];
+				}
+				return statics;
+			})
+			.then(statics => [templates, filepaths, statics]))
+		.then(([templates, filepaths, statics]) =>
+			renderPages(filepaths, opts.dest, {templates, vars: opts.vars, statics, disableValidation: opts.disableValidation, cssVariables: opts.cssVariables, host})
+				.then(pageStylesheets => {
+					if (opts.scopedCss) {
+						const rules = pageStylesheets.join('\n').split('}').map(rule => rule.trim() + '}');
+						const uniqueRules = Array.from(new Set(rules.slice(0, rules.length - 1))).join('\n');
+						return sander.writeFile(opts.scopedCss, uniqueRules);
+					}
+				}))
+		.then(() => createRedirects(host, opts.redirectMap, opts.dest))
 		.then(() => {
 			console.log(`\n${chalk.bold.green('SUCCESS')}\n`);
 		})
@@ -23,35 +61,4 @@ function logResult(promise) {
 			console.error(err);
 			throw err;
 		});
-}
-
-function build(opts) {
-	const {srcPages, srcTemplates, srcStatics, srcHelpers, dest, destStatics, vars, disableValidation,
-		redirectMap, scopedCss, cssVariables, templateImport} = opts;
-	const promise =
-		sander.copydir(path.join(process.cwd(), srcStatics)).to(path.join(process.cwd(), destStatics))
-			.catch(() => {/* just ignore missing statics folder */})
-			.then(() => createTemplates(srcTemplates, srcHelpers, {cssVariables}))
-			.then(templates => mixinExternalTemplates(templateImport, templates))
-			.then(templates => globby([srcPages]).then(filepaths => [templates, filepaths]))
-			.then(([templates, filepaths]) => globby([path.join(destStatics, '**')])
-				.then(statics => {
-					// If we need to write css, then implicitly count it to static resources
-					if (scopedCss) {
-						return [...statics, scopedCss.replace(/^.\//, '')];
-					}
-					return statics;
-				})
-				.then(statics => [templates, filepaths, statics]))
-			.then(([templates, filepaths, statics]) =>
-				renderPages(filepaths, dest, {templates, vars, statics, disableValidation, cssVariables, host})
-					.then(pageStylesheets => {
-						if (scopedCss) {
-							const rules = pageStylesheets.join('\n').split('}').map(rule => rule.trim() + '}');
-							const uniqueRules = Array.from(new Set(rules.slice(0, rules.length - 1))).join('\n');
-							return sander.writeFile(scopedCss, uniqueRules);
-						}
-					}))
-			.then(() => createRedirects(host, redirectMap, dest));
-	return logResult(promise);
 }
